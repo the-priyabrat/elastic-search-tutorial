9118e7a7ca323ff88e727279dab503a2a7788106
the-priyabrat <priyabrataswain1338@gmail.com>
2026-07-08 23:09:12 +0530

    Docker error resolve, and added health check

--- src/main/java/com/project/elasticSearch/config/KafkaTopicConfig.java
+++ src/main/java/com/project/elasticSearch/config/KafkaTopicConfig.java
@@ -1,9 +1,11 @@
 package com.project.elasticSearch.config;
 
 import com.project.elasticSearch.dto.KafkaResponse;
+import jakarta.annotation.PostConstruct;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
